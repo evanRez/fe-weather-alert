666a0296bf3d5e154560eb3c0e1201dbbe50ef12
evan <ereznice@gmail.com>
2024-02-20 21:10:09 +0000

    messing with the dockerfile so I can build the image as amd64

--- fe-weatheralert/src/vite-env.d.ts
+++ fe-weatheralert/src/vite-env.d.ts
@@ -1,10 +1,2 @@
 /// <reference types="vite/client" />
 
-type ninjaCity = {
-    'country': string,
-    'is_capital': boolean,
-    'latitude': number,
-    'longitude': number,
-    'name': string,
-    'population': number
-}
